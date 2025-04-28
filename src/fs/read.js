2b566a38ac7762b1799eecd4a9903c5a8de7291d
nadyasav <nadyasav28@gmail.com>
2025-04-28 22:46:23 +0300

    feat(fs): implement read task

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,19 @@
+import { fileURLToPath } from 'node:url';
+import path from 'node:path';
+import fs from 'node:fs/promises';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+    const filePath = path.resolve(__dirname, 'files', 'fileToRead.txt');
+
+    try{
+        const fileData = await fs.readFile(filePath, { encoding: 'utf8' });
+        console.log(fileData);
+    } catch {
+        throw new Error('FS operation failed');
+    }
 };
 
 await read();
