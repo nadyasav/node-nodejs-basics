9778ff6831f19d4fa61a297cb6aadfe86b41585e
nadyasav <nadyasav28@gmail.com>
2025-04-28 22:59:53 +0300

    feat(fs): implement list task

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,21 @@
+import { fileURLToPath } from 'node:url';
+import path from 'node:path';
+import fs from 'node:fs/promises';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const list = async () => {
-    // Write your code here 
+    const filesFolderPath = path.resolve(__dirname, 'files');
+
+    try{
+        const filesArr = await fs.readdir(filesFolderPath);
+        filesArr.forEach((fileName) => {
+            console.log(fileName);
+        })
+    } catch {
+        throw new Error('FS operation failed');
+    }
 };
 
 await list();
